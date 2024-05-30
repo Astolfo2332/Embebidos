5e651f2678dc1534e59017ab61044153d0ef3b1e
astolfo_arch <migue19sep@gmail.com>
2024-05-29 20:26:16 -0500

    Adicion de tcp y cambiar las variables sin config

---
+++ Trabajo2/tcp_client_main.c
@@ -0,0 +1,27 @@
+/*
+ * SPDX-FileCopyrightText: 2022 Espressif Systems (Shanghai) CO LTD
+ *
+ * SPDX-License-Identifier: Unlicense OR CC0-1.0
+ */
+#include "nvs_flash.h"
+#include "esp_netif.h"
+#include "protocol_examples_common.h"
+#include "esp_event.h"
+#include "wifi_conection.h"
+
+
+
+void tcp_client(uint16_t newPort, char* newIpHost);
+
+void app_main(void)
+{
+    
+    wifi_conection("VELEZ","42880670");
+
+    /* This helper function configures Wi-Fi or Ethernet, as selected in menuconfig.
+     * Read "Establishing Wi-Fi or Ethernet Connection" section in
+     * examples/protocols/README.md for more information about this function.
+     */
+
+    tcp_client(3333,"192.168.1.12");
+}
